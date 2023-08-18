299eb9364afe875fef2c7bd26dd02f097a20ee2a
VinodChaudhari908 <vinodchaudhary0580@gmail.com>
2023-08-18 07:12:34 -0700

    first added

--- raw.java
+++ raw.java
@@ -1,4 +1,8 @@
 public class raw {
+    public static void first() {
+        System.out.println("first");
+    }
+
     public static void main(String[] args) {
         System.out.println("raw");
     }
