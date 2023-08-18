7431823cbc92c540a500964d90cd291179dbbd68
VinodChaudhari908 <vinodchaudhary0580@gmail.com>
2023-08-18 06:07:15 -0700

    subs function  added

--- demo.java
+++ demo.java
@@ -12,6 +12,10 @@ public class demo {
 
     }
 
+    public static void subs() {
+        // substraction function added
+    }
+
     public static void main(String[] args) {
         System.out.println("hello vinod ");
         System.out.println("i am learning git  tutorial !!!");
