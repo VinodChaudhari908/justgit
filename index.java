e26f280d41fb0e7fd11b00c08823a706f477bb14
VinodChaudhari908 <vinodchaudhary0580@gmail.com>
2023-08-18 06:34:35 -0700

    hello added

--- index.java
+++ index.java
@@ -2,4 +2,9 @@ public class index {
     public static void main(String[] args) {
         System.out.println("index");
     }
+
+    public static void hello() {
+        // hello function added
+        System.out.println("hello vinod ");
+    }
 }
