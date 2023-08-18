e280a0fc835403ee1fa009b5b9d68742c5dd9479
VinodChaudhari908 <vinodchaudhary0580@gmail.com>
2023-08-18 07:14:53 -0700

    second added

--- raw.java
+++ raw.java
@@ -1,4 +1,9 @@
 public class raw {
+    public static void second() {
+        // second function added
+        System.out.println("first");
+    }
+
     public static void main(String[] args) {
         System.out.println("raw");
     }
