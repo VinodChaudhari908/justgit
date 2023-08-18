f6e214e16b02eecbce0939848361f56eea921288
VinodChaudhari908 <vinodchaudhary0580@gmail.com>
2023-08-18 05:57:56 -0700

    multiply

--- demo.java
+++ demo.java
@@ -3,6 +3,12 @@
  */
 public class demo {
 
+    // multiply function
+    public static void multiply() {
+        // multiply function
+
+    }
+
     public static void main(String[] args) {
         System.out.println("hello vinod ");
         System.out.println("i am learning git  tutorial !!!");
