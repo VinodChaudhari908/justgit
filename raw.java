6277dcd4796d8c6f3e8a5c2c2c564e81061b4bff
VinodChaudhari908 <vinodchaudhary0580@gmail.com>
2023-08-18 21:54:06 -0700

    Prime of given number

--- raw.java
+++ raw.java
@@ -13,4 +13,9 @@ public class raw {
     public static void main(String[] args) {
         System.out.println("raw");
     }
+
+    public static void prime() {
+        // for finding prime of given number
+
+    }
 }
