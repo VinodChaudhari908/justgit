bcc0c41a31370813892bc51c96475b631df21245
Vinod Kumar Chaudhari <vinodchaudhary0580@gmail.com>
2023-08-21 23:50:11 -0700

    brnch1 iniatiated

--- index.java
+++ index.java
@@ -6,4 +6,8 @@ public class index {
     public static void main(String[] args) {
         System.out.println("hello vinod kumar chaudhari");
     }
+
+    public static void hello() {
+        System.out.println("sasrical how are you?");
+    }
 }
