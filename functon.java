673526c72304e11625622b30e70b0c4aa326c9e0
Vinod Kumar Chaudhari <vinodchaudhary0580@gmail.com>
2023-09-01 19:22:50 -0700

    function got staged

---
+++ functon.java
@@ -0,0 +1,10 @@
+//this is my function in java programming language 
+/**
+ * functon
+ */
+public class functon {
+
+    public static void main(String[] args) {
+        System.out.println("hello harry how are you !!!");
+    }
+}
